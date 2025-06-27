d1030513e6ae69e55b2e9135c3f4e0cfae1288a3
Przemysław Jacewicz <przemyslaw.jacewicz@proton.me>
2025-06-28 00:56:54 +0200

    update

---
+++ src/test/scala/pl/epsilondeltalimit/dep/DepSpec.scala
@@ -0,0 +1,290 @@
+package pl.epsilondeltalimit.dep
+
+import org.scalactic.Equality
+import org.scalactic.Equality.{default => _}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import pl.epsilondeltalimit.dep.catalog.Catalog
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration
+import scala.concurrent.duration.Duration
+
+class DepSpec extends AnyFlatSpec with Matchers {
+
+  implicit val depEq: Equality[Dep[Int]] = (a: Dep[Int], b: Any) => {
+    println(
+      s"[depEq]: a.class=${a.getClass.getSimpleName}, id=${a.id}, needs=${a.needs().mkString(",")}, value=${a.value()}")
+    val bDep = b.asInstanceOf[Dep[_]]
+    println(
+      s"[depEq]: b.class=${b.getClass.getSimpleName}, id=${bDep.id}, needs=${bDep.needs().mkString(",")}, value=${bDep.value()}")
+    b match {
+      case d: Dep[_] => a.getClass == d.getClass && a.id == d.id && a.needs() == d.needs() && a.value() == d.value()
+      case _           => false
+    }
+  }
+
+  implicit val resultDepEq: Equality[Result[Int]] = (a: Result[Int], b: Any) => {
+    println(
+      s"[resultDepEq]: a.class=${a.getClass.getSimpleName}, id=${a.id}, needs=${a.needs().mkString(",")}, value=${a.value()}")
+    val bDep = b.asInstanceOf[Dep[_]]
+    println(
+      s"[resultDepEq]: b.class=${b.getClass.getSimpleName}, id=${bDep.id}, needs=${bDep.needs().mkString(",")}, value=${bDep
+        .value()}")
+    b match {
+      case d: Result[_] => a.id == d.id && a.needs() == d.needs() && a.value() == d.value()
+      case _              => false
+    }
+  }
+
+  implicit val partDepEq: Equality[Part[Int]] = (a: Part[Int], b: Any) => {
+    println(
+      s"[partDepEq]: a.class=${a.getClass.getSimpleName}, id=${a.id}, needs=${a.needs().mkString(",")}, value=${a.value()}")
+    val bDep = b.asInstanceOf[Dep[_]]
+    println(
+      s"[partDepEq]: b.class=${b.getClass.getSimpleName}, id=${bDep.id}, needs=${bDep.needs().mkString(",")}, value=${bDep
+        .value()}")
+    b match {
+      case d: Part[_] => a.id == d.id && a.needs() == d.needs() && a.value() == d.value()
+      case _            => false
+    }
+  }
+
+  behavior of "apply"
+
+  it should "cache the result" in {
+
+    def time(block: => Unit): Long = {
+      val start = System.nanoTime()
+      block
+      val end = System.nanoTime()
+      end - start
+    }
+
+    val dep = Result[Unit]("dep", () => Set.empty, () => TimeUnit.SECONDS.sleep(1))
+
+    time(dep()) should be > Duration(1, duration.SECONDS).toNanos
+    time(dep()) should be < Duration(1, duration.SECONDS).toNanos
+  }
+
+  behavior of "map"
+
+  val t: Catalog => Catalog = (_: Catalog).put("u")(1)
+
+  it should "create a Dep instance with proper id and needs" in {
+    info("map")
+    Result("id", () => Set("u"), () => 1).map(_ + 1) should ===(Part("id_M", () => Set("u", "id"), () => 2))
+    Part("id", () => Set("u"), () => 1).map(_ + 1) should ===(Part("id_M", () => Set("u"), () => 2))
+
+    info("map + as")
+    Result("id", () => Set("u"), () => 1).map(_ + 1).as("t") should ===(Result("t", () => Set("u", "id"), () => 2))
+    Part("id", () => Set("u"), () => 1).map(_ + 1).as("t") should ===(Result("t", () => Set("u"), () => 2))
+
+    info("map + map")
+    Result("id", () => Set("u"), () => 1).map(_ + 1).map(_ + 1) should ===(
+      Part("id_M_M", () => Set("u", "id"), () => 3))
+    Part("id", () => Set("u"), () => 1).map(_ + 1).map(_ + 1) should ===(Part("id_M_M", () => Set("u"), () => 3))
+
+    info("map + map + as")
+    Result("id", () => Set("u"), () => 1).map(_ + 1).map(_ + 1).as("t") should ===(
+      Result("t", () => Set("u", "id"), () => 3))
+    Part("id", () => Set("u"), () => 1).map(_ + 1).map(_ + 1).as("t") should ===(Result("t", () => Set("u"), () => 3))
+  }
+
+  behavior of "flatMap"
+
+  it should "create a Dep instance with proper id and needs" in {
+    info("flatMap")
+    Result("id", () => Set("u"), () => 1).flatMap(i => Result("id2", () => Set("u2"), () => i + 1)) should ===(
+      Part("id_FM", () => Set("u", "id", "u2", "id2"), () => 2))
+    Result("id", () => Set("u"), () => 1).flatMap(i => Part("id2", () => Set("u2"), () => i + 1)) should ===(
+      Part("id_FM", () => Set("u", "id", "u2"), () => 2))
+
+    Part("id", () => Set("u"), () => 1).flatMap(i => Result("id2", () => Set("u2"), () => i + 1)) should ===(
+      Part("id_FM", () => Set("u", "u2", "id2"), () => 2))
+    Part("id", () => Set("u"), () => 1).flatMap(i => Part("id2", () => Set("u2"), () => i + 1)) should ===(
+      Part("id_FM", () => Set("u", "u2"), () => 2))
+
+    info("flatMap + as")
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "id2"), () => 2))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2"), () => 2))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2", "id2"), () => 2))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2"), () => 2))
+
+    info("flatMap + flatMap")
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "id", "u2", "id2", "u3", "id3"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "id", "u2", "id2", "u3"), () => 3))
+
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "id", "u2", "u3", "id3"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "id", "u2", "u3"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "u2", "id2", "u3", "id3"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "u2", "id2", "u3"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "u2", "u3", "id3"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1)) should ===(
+      Part("id_FM_FM", () => Set("u", "u2", "u3"), () => 3))
+
+    info("flatMap + flatMap + as")
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "id2", "u3", "id3"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "id2", "u3"), () => 3))
+
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "u3", "id3"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "u3"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2", "id2", "u3", "id3"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2", "id2", "u3"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Result("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2", "u3", "id3"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .flatMap(j => Part("id3", () => Set("u3"), () => j + 1))
+      .as("t") should ===(Result("t", () => Set("u", "u2", "u3"), () => 3))
+
+    info("flatMap + map")
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "id", "u2", "id2"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "id", "u2", "id2"), () => 3))
+
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "id", "u2"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "id", "u2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "u2", "id2"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "u2", "id2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "u2"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1) should ===(Part("id_FM_M", () => Set("u", "u2"), () => 3))
+
+    info("flatMap + map + as")
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "id2"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2", "id2"), () => 3))
+
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2"), () => 3))
+    Result("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "id", "u2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "u2", "id2"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Result("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "u2", "id2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "u2"), () => 3))
+    Part("id", () => Set("u"), () => 1)
+      .flatMap(i => Part("id2", () => Set("u2"), () => i + 1))
+      .map(_ + 1)
+      .as("t") should ===(Result("t", () => Set("u", "u2"), () => 3))
+  }
+
+  behavior of "map2"
+
+  it should "create a Dep instance with proper id and needs" in {
+    info("map2")
+    Result("id", () => Set("u"), () => 1).map2(Result("id2", () => Set("u2"), () => 2))(_ + _) should ===(
+      Part("id_M2", () => Set("u", "id", "u2", "id2"), () => 3))
+    Result("id", () => Set("u"), () => 1).map2(Part("id2", () => Set("u2"), () => 2))(_ + _) should ===(
+      Part("id_M2", () => Set("u", "id", "u2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1).map2(Result("id2", () => Set("u2"), () => 2))(_ + _) should ===(
+      Part("id_M2", () => Set("u", "u2", "id2"), () => 3))
+    Part("id", () => Set("u"), () => 1).map2(Part("id2", () => Set("u2"), () => 2))(_ + _) should ===(
+      Part("id_M2", () => Set("u", "u2"), () => 3))
+
+    info("map2 + as")
+    Result("id", () => Set("u"), () => 1).map2(Result("id2", () => Set("u2"), () => 2))(_ + _).as("t") should ===(
+      Result("t", () => Set("u", "id", "u2", "id2"), () => 3))
+    Result("id", () => Set("u"), () => 1).map2(Part("id2", () => Set("u2"), () => 2))(_ + _).as("t") should ===(
+      Result("t", () => Set("u", "id", "u2"), () => 3))
+
+    Part("id", () => Set("u"), () => 1).map2(Result("id2", () => Set("u2"), () => 2))(_ + _).as("t") should ===(
+      Result("t", () => Set("u", "u2", "id2"), () => 3))
+    Part("id", () => Set("u"), () => 1).map2(Part("id2", () => Set("u2"), () => 2))(_ + _).as("t") should ===(
+      Result("t", () => Set("u", "u2"), () => 3))
+  }
+
+}
